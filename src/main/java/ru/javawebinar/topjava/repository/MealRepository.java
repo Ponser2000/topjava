ca3d413884abf3ef181a6877b6d3ace897eafd5b
Ponser2000 <ponser2000@gmail.com>
2021-02-11 18:39:02 +0300

    prepare HW2

---
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.repository;
+
+import java.util.Collection;
+import ru.javawebinar.topjava.model.Meal;
+
+public interface MealRepository {
+    // null if updated meal do not belong to userId
+    Meal save(Meal meal);
+
+    // false if meal do not belong to userId
+    boolean delete(int id);
+
+    // null if meal do not belong to userId
+    Meal get(int id);
+
+    // ORDERED dateTime desc
+    Collection<Meal> getAll();
+}
---
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -0,0 +1,43 @@
+package ru.javawebinar.topjava.service;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User create(User user) {
+        return repository.save(user);
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public User get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public User getByEmail(String email) {
+        return checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
+
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+
+    public void update(User user) {
+        checkNotFoundWithId(repository.save(user), user.getId());
+    }
+}
