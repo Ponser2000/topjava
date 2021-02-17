7107f15d56d869f405fa0addcd946e4bf1a7ddc7
Ponser2000 <ponser2000@gmail.com>
2021-02-17 11:04:33 +0300

    HW02 stage 1 & 2

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,45 +1,40 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.MealsUtil;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
-    private final AtomicInteger counter = new AtomicInteger(0);
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
 
-    {
-        MealsUtil.meals.forEach(this::save);
-    }
 
     @Override
     public Meal save(Meal meal) {
-        if (meal.isNew()) {
-            meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
-            return meal;
-        }
-        // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        log.info("save {}", meal);
+        return meal;
     }
 
     @Override
     public boolean delete(int id) {
-        return repository.remove(id) != null;
+        log.info("delete {}", id);
+        return true;
     }
 
     @Override
     public Meal get(int id) {
-        return repository.get(id);
+        log.info("get {}", id);
+        return null;
     }
 
     @Override
     public Collection<Meal> getAll() {
-        return repository.values();
+        log.info("getAll");
+        return Collections.emptyList();
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -3,7 +3,9 @@ package ru.javawebinar.topjava.service;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
@@ -11,33 +13,35 @@ import ru.javawebinar.topjava.repository.UserRepository;
 @Service
 public class UserService {
 
-    private final UserRepository repository;
+  private final UserRepository repository;
 
-    public UserService(UserRepository repository) {
-        this.repository = repository;
-    }
+  public UserService(UserRepository repository) {
+    this.repository = repository;
+  }
 
-    public User create(User user) {
-        return repository.save(user);
-    }
+  public User create(User user) {
+    return repository.save(user);
+  }
 
-    public void delete(int id) {
-        checkNotFoundWithId(repository.delete(id), id);
-    }
+  public void delete(int id) {
+    checkNotFoundWithId(repository.delete(id), id);
+  }
 
-    public User get(int id) {
-        return checkNotFoundWithId(repository.get(id), id);
-    }
+  public User get(int id) {
+    return checkNotFoundWithId(repository.get(id), id);
+  }
 
-    public User getByEmail(String email) {
-        return checkNotFound(repository.getByEmail(email), "email=" + email);
-    }
+  public User getByEmail(String email) {
+    return checkNotFound(repository.getByEmail(email), "email=" + email);
+  }
 
-    public List<User> getAll() {
-        return repository.getAll();
-    }
+  public List<User> getAll() {
+    return repository.getAll().stream()
+        .sorted(Comparator.comparing(User::getName))
+        .collect(Collectors.toList());
+  }
 
-    public void update(User user) {
-        checkNotFoundWithId(repository.save(user), user.getId());
-    }
+  public void update(User user) {
+    checkNotFoundWithId(repository.save(user), user.getId());
+  }
 }
