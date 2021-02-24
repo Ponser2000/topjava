7e954105a850647053b5f21876afb3b13290c00f
Ponser2000 <ponser2000@gmail.com>
2021-02-24 10:15:23 +0300

    3_0_fix

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -4,13 +4,13 @@ import java.util.Collection;
 import ru.javawebinar.topjava.model.Meal;
 
 public interface MealRepository {
-    // null if updated meal do not belong to userId
+    // null if updated meal does not belong to userId
     Meal save(Meal meal);
 
-    // false if meal do not belong to userId
+    // false if meal does not belong to userId
     boolean delete(int id);
 
-    // null if meal do not belong to userId
+    // null if meal does not belong to userId
     Meal get(int id);
 
     // ORDERED dateTime desc
